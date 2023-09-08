776599d47df463006fe08be8ce91b497dccdfd3e
Gabriel Pedro <gabrielpedrodsantos@gmail.com>
2023-09-08 16:54:06 -0300

    alterei o objeto sc da classe Scanner para scanner

--- ContaBanco/src/contaBancaria/ContaTerminal.java
+++ ContaBanco/src/contaBancaria/ContaTerminal.java
@@ -9,16 +9,16 @@ public class ContaTerminal {
 		int numeroConta;
 		String agencia, nomeCliente;
 		double saldo;
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         
         System.out.println("Por favor, digite o número da conta: ");
-        numeroConta = sc.nextInt();
+        numeroConta = scanner.nextInt();
         System.out.println("Agora, digite o número da agência: ");
-        agencia = sc.next();
+        agencia = scanner.next();
         System.out.println("Insira agora o seu nome: ");
-        nomeCliente = sc.next();
+        nomeCliente = scanner.next();
         System.out.println("Por fim, insira o saldo da conta: ");
-        saldo = sc.nextDouble();
+        saldo = scanner.nextDouble();
         
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
         + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque"); 
