59e98ffc0d10843903e3cbe3abf1e07935591993
Gabriel Pedro <gabrielpedrodsantos@gmail.com>
2023-09-08 16:39:43 -0300

    projeto ContaBanco finalizado

--- ContaBanco/src/contaBancaria/ContaTerminal.java
+++ ContaBanco/src/contaBancaria/ContaTerminal.java
@@ -1,8 +1,27 @@
 package contaBancaria;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
+		
+		int numeroConta;
+		String agencia, nomeCliente;
+		double saldo;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o número da conta: ");
+        numeroConta = sc.nextInt();
+        System.out.println("Agora, digite o número da agência: ");
+        agencia = sc.next();
+        System.out.println("Insira agora o seu nome: ");
+        nomeCliente = sc.next();
+        System.out.println("Por fim, insira o saldo da conta: ");
+        saldo = sc.nextDouble();
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+        + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque"); 
 		
 	}
 	
